e867a31e7c7f5fb2ff3a54b3a2a76403b95005aa
Nichoas Esterer <nicholas.esterer@gmail.com>
2016-03-02 19:31:30 -0500

    Adjust plot_raw_sound script so plotting is optional.

--- scripts/plot_raw_sound.m
+++ scripts/plot_raw_sound.m
@@ -1,4 +1,8 @@
-function [x] = plot_raw_sound(path,format="int16",channels=2,channel=1)
+function [x] = plot_raw_sound(path,
+                              format="int16",
+                              channels=2,
+                              channel=1,
+                              do_plot=1)
 % PLOT_RAW_SOUND
 % path = path to file
 % format = the format of the samples (default="int16")
@@ -6,10 +10,15 @@ function [x] = plot_raw_sound(path,format="int16",channels=2,channel=1)
 %   (default=2)
 % channel = the channel that will be plotted. Indexing starts at 1.
 %   (default=1)
+% do_plot = whether or not to display the plot (can be used to just get the
+%   values from a file). 0 is false, nonzero is true.
+%   (default=1)
 % 
 % Returns
 % The signal that it plotted.
 f=fopen(path,"r");
 x=fread(f,Inf,format);
 x=x(channel:channels:end);
-plot(0:(length(x)-1),x);
+if (do_plot)
+    plot(0:(length(x)-1),x);
+end
