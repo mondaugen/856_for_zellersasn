c29e06cb9c75f41fae9493e6c6233cb59940de6e
Nichoas Esterer <nicholas.esterer@gmail.com>
2016-11-15 16:31:15 -0500

    Switch test uses addresses gotten via GDB so should be more accurate.
    Doesn't test using comparisons, but reflects clearly the state of the switches.

--- scripts/get_switch_port_addrs.pl
+++ scripts/get_switch_port_addrs.pl
@@ -3,9 +3,11 @@ BEGIN{ $l1=""; $l2="";}
 ($port,$pin) = ($_ =~ m/    return \((\(\(GPIO_.*IDR) & \(0x1 << ([0-9]+)\)/);
 if ($port){ 
     ($tmp) = ($l2 =~ m/uint32_t (.*)\_get_state.*/);
-    print "set \$tmp = &".$port."\n";
-    print "printf \"".$tmp."_reg {%#x}\\n\", \$tmp \n";
-    print "printf \"".$tmp."_pin {".$pin."}\\n\"\n";
+    if ($tmp =~ m/sw[0-9]+/) {
+        print "set \$tmp = &".$port."\n";
+        print "printf \"".$tmp."_reg {%#x}\\n\", \$tmp \n";
+        print "printf \"".$tmp."_pin {".$pin."}\\n\"\n";
+    }
 }
 $l2=$l1;
 $l1=$_;
--- scripts/test_switch.tcl
+++ scripts/test_switch.tcl
@@ -1,75 +1,61 @@
 init
-array set swreg [list \
-    {sw1_btm_reg} {0x40021810} \
-    {sw2_btm_reg} {0x40020c10} \
-    {sw3_btm_reg} {0x40020410} \
-    {sw4_btm_reg} {0x40020410} \
-    {sw5_btm_reg} {0x40020c10} \
-    {sw6_btm_reg} {0x40020c10} \
-    {sw7_btm_reg} {0x40020810} \
-    {sw8_btm_reg} {0x40020810} \
-    \
-    {sw1_top_reg} {0x40021810} \
-    {sw2_top_reg} {0x40020c10} \
-    {sw3_top_reg} {0x40020c10} \
-    {sw4_top_reg} {0x40020410} \
-    {sw5_top_reg} {0x40020410} \
-    {sw6_top_reg} {0x40020c10} \
-    {sw7_top_reg} {0x40020c10} \
-    {sw8_top_reg} {0x40021810} \
-    \
-    {sw1_btm_pin} {0x3} \
-    {sw2_btm_pin} {0xd} \
-    {sw3_btm_pin} {0x0} \
-    {sw4_btm_pin} {0x7} \
-    {sw5_btm_pin} {0x7} \
-    {sw6_btm_pin} {0x2} \
-    {sw7_btm_pin} {0x8} \
-    {sw8_btm_pin} {0x6} \
-    \
-    {sw1_top_pin} {0x2} \
-    {sw2_top_pin} {0xc} \
-    {sw3_top_pin} {0xb} \
-    {sw4_top_pin} {0x1} \
-    {sw5_top_pin} {0x8} \
-    {sw6_top_pin} {0x4} \
-    {sw7_top_pin} {0x3} \
-    {sw8_top_pin} {0x9} \
-    ]
+signal handle SIGINT
 
-set regnums [range 1 9 1]
+# clear screen
+puts -nonewline "\033\x5b2J"
 
-foreach i $regnums {
-
-    set top_reg_ $swreg([format "sw%d_top_reg" $i])
-    set btm_reg_ $swreg([format "sw%d_btm_reg" $i])
-    set top_pin_ $swreg([format "sw%d_top_pin" $i])
-    set btm_pin_ $swreg([format "sw%d_btm_pin" $i])
-
-    puts [format "Put switch %d in top position" $i]
-    puts "Press enter to test."
-    gets stdin
-
-    mem2array x 32 $btm_reg_ 1
-    if {[expr ($x(0) & (1 << $btm_pin_)) >> $btm_pin_] == 0} {
-        puts "passed"
-    } else {
-        puts "failed"
+while {[catch -signal {
+    array set swreg [source /tmp/switch_vals_addr.XXX]
+    
+    set regnums [range 1 11 1]
+    set reg_phys_map [list 4 0 6 1 5 2 7 3]
+    set rslt_btm [list 1 2 3 4 5 6 7 8]
+    set rslt_top [list 1 2 3 4 5 6 7 8]
+    set fsw_regnums [range 1 3]
+    set rslt_fsw [range 1 3]
+    
+    foreach i [lrange $regnums 0 7] {
+    
+        set top_reg_ $swreg([format "sw%d_top_reg" $i])
+        set btm_reg_ $swreg([format "sw%d_btm_reg" $i])
+        set top_pin_ $swreg([format "sw%d_top_pin" $i])
+        set btm_pin_ $swreg([format "sw%d_btm_pin" $i])
+        set phs_map [lindex $reg_phys_map [expr {$i - 1}]]
+        #    puts [format "%d" $phs_map]
+        mem2array x 32 $btm_reg_ 1
+        lset rslt_btm $phs_map [expr {($x(0) & (1 << $btm_pin_)) >> $btm_pin_}]
+        mem2array x 32 $top_reg_ 1
+        lset rslt_top $phs_map [expr {($x(0) & (1 << $top_pin_)) >> $top_pin_}]
+    
     }
-    puts [format "%d" [expr ($x(0) & (1 << $btm_pin_)) >> $btm_pin_]]
-
-    puts [format "Put switch %d in bottom position" $i]
-    puts "Press enter to test."
-    gets stdin
-
-    mem2array y 32 $top_reg_ 1
-    if {[expr ($y(0) & (1 << $top_pin_)) >> $top_pin_] == 0} {
-        puts "passed"
-    } else {
-        puts "failed"
+    
+    foreach i $fsw_regnums {
+        set top_reg_ $swreg([format "fsw%d_reg" $i])
+        set top_pin_ $swreg([format "fsw%d_pin" $i])
+        mem2array x 32 $top_reg_ 1
+        lset rslt_fsw [expr {$i-1}] \
+        [expr {($x(0) & (1 << $top_pin_)) >> $top_pin_}]
+    
     }
-    puts [format "%d" [expr ($y(0) & (1 << $top_pin_)) >> $top_pin_]]
 
+    # move cursor to 1,1 position (top left-hand corner)
+    puts -nonewline "\033\x5b1;1H"
+    
+    set outstr ""
+    
+    append outstr [format " %d %d %d %d\n" {*}[lrange $rslt_btm 0 3]]
+    append outstr [format " %d %d %d %d\n" {*}[lrange $rslt_top 0 3]]
+    append outstr [format "%d %d %d %d\n" {*}[lrange $rslt_btm 4 7]]
+    append outstr [format "%d %d %d %d\n" {*}[lrange $rslt_top 4 7]]
+    append outstr [format "%d      %d"    {*}$rslt_fsw]
+    
+    puts $outstr
+
+    }] == 0} {
+    # move cursor to 1,1 position (top left-hand corner)
+    puts -nonewline "\033\x5b1;1H"
+    after 500
 }
+
 
 shutdown
